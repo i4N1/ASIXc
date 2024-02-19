66d7ccfacd02c055a424e909557ace89878ce723
i4N1 <iandiazp.01@gmail.com>
2024-02-19 10:37:07 +0100

    ps hw todo exTar

---
+++ PowerShell/exTar.ps1
@@ -0,0 +1,18 @@
+﻿if ($args.Length -ne 2){
+    Write-Host "Needed $(2 - $($args.Length)) more arguments."
+    exit 1
+}
+$orig = $args[0]
+$dest = $args[1]
+$date = Get-Date -Format "yyyyMMdd"
+$dest = Join-Path -Path $dest -ChildPath "backup_$(Split-Path -Leaf $orig)_$date"
+Write-Host $dest
+if (Test-Path $dest -PathType Container){
+    Write-Host '$dest already exists.'
+    exit 2
+}
+
+if (-not (Test-Path $orig -PathType Container)){
+    Write-Host '$orig not found.'
+    exit 3
+}
