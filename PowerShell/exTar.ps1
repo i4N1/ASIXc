de0106ed745a685cd1bf6537674d814aee6fc721
i4N1 <iandiazp.01@gmail.com>
2024-02-19 22:19:39 +0100

    extar

--- PowerShell/exTar.ps1
+++ PowerShell/exTar.ps1
@@ -1,18 +1,25 @@
 ﻿if ($args.Length -ne 2){
     Write-Host "Needed $(2 - $($args.Length)) more arguments."
-    exit 1
+    exit 3
 }
 $orig = $args[0]
 $dest = $args[1]
 $date = Get-Date -Format "yyyyMMdd"
-$dest = Join-Path -Path $dest -ChildPath "backup_$(Split-Path -Leaf $orig)_$date"
-Write-Host $dest
+$dest = Join-Path -Path $dest -ChildPath "backup_$(Split-Path -Leaf $orig)_$date.zip"
+
 if (Test-Path $dest -PathType Container){
     Write-Host '$dest already exists.'
     exit 2
 }
 
-if (-not (Test-Path $orig -PathType Container)){
+elseif (-not (Test-Path $orig -PathType Container)){
     Write-Host '$orig not found.'
-    exit 3
+    exit 1
+}
+
+else{
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($orig, $dest)
+
+    Write-Host "Backup ready: $dest"
 }
